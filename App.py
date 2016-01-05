995b92d7de2a42c52547f949b3e7d8f1ebf3773c
tom@cgfindies.co.uk <tom@cgfindies.co.uk>
2016-01-05 22:28:23 +0000

    Started creating documentation. Hopefully this is of help to somebody. More to come soon.

--- App.py
+++ App.py
@@ -3,6 +3,7 @@ import tornado.web
 import os.path
 import tornado.httpserver
 
+from Controller.BaseHandler import BaseHandler
 from Controller.NotFoundHandler import NotFoundHandler
 from Controller.IndexHandler import IndexHandler
 from Controller.ViewHandler import ViewHandler
@@ -12,7 +13,38 @@ from Controller.DeleteHandler import DeleteHandler
 from Module.SinglePost.SinglePost import SinglePost
 
 class Application(tornado.web.Application):
+    """
+        This is the entry point for the app.
+        The Tornado docs show initialising it a slightly different way, but I couldn't get the settings to register that way.
+        It might have been me mistaking the syntax, but this way definitely works.
+        Set up the Handlers (these would be the Controllers in an MVC app) here, as well as app settings.
+        An instance of this app is used to start the Tornado web server (see below)
+        
+        @ivar handlers: These are the request handlers.
+        When a URL request comes in, it is compared against this regular expressions in the list and.
+        If the url matches a pattern, the specified L{tornado.web.RequestHandler} subclass is used for the request.
+        RequestHandlers are selected as first-come, first-served. As soon as a match is found, that handler is used and the app doesn't try to match further RequestHandlers.
+        I just used the pattern and handler parts of URLSpec, but there's also kwargs and name paramaters that you might want to check out.
+        These are all subclasses of L{BaseHandler}, which is a subclass of L{tornado.web.RequestHandler}
+        These subclasses define the methods that the subclass can handle (get, post, etc.).
+        This is important if you don't want the app throwing a 405 Method Not Allowed http error.
+        @type handlers: [L{tornado.web.URLSpec}]
+        
+        @ivar settings: These are settings for the Tornado app.
+        I've included a few here, but a full list is avaliable at U{http://www.tornadoweb.org/en/stable/web.html#tornado.web.Application.settings}.
+        This sets up:
+            - The default path for template/view files used to render pages
+            - A list of re-usable ui elements (UIModules).
+            Each one is a Subclass of U{http://www.tornadoweb.org/en/stable/web.html#tornado.web.UIModule}
+            - Cross Site Request Forgery protection (see U{http://www.tornadoweb.org/en/stable/guide/security.html?highlight=xsrf#cross-site-request-forgery-protection})
+            - A ridiculously weak cookie secret key, you should change this. See (U{http://www.tornadoweb.org/en/stable/guide/security.html?highlight=xsrf#cookies-and-secure-cookies})
+            - Turning debug mode on. This sets several other settings. Turn this off before going into production.
+        See U{http://www.tornadoweb.org/en/stable/guide/running.html#debug-mode-and-automatic-reloading}
+        @type settings: dict
+    """
+    
     def __init__(self):
+        
         handlers = [
             (r"/", IndexHandler),                               #GET retrieve all posts
             (r"/get/post/([0-9]+)/", ViewHandler),              #GET retrieve single post
@@ -22,6 +54,7 @@ class Application(tornado.web.Application):
             (r"/post/post/([0-9]+)/", PostHandler),             #GET or POST update single post
             (r"/(.*)", NotFoundHandler),                        #404 links and everything else
         ];
+        
         settings = dict(
             template_path=os.path.join(os.path.dirname(__file__), "View"),
             ui_modules={"SinglePost": SinglePost},
@@ -29,8 +62,15 @@ class Application(tornado.web.Application):
             cookie_secret="Swordfish",
             debug=True,
         )
+        
+        # Initialize the app, passing in the handlers and settings.
         super(Application, self).__init__(handlers, **settings)
 
+"""
+    This is how you start the tornado web server.
+    Use L{tornado.web.httpserver.HTTPServer(L{tornado.web.Application})<tornado.web.httpserver.HTTPServer>} to get a reference to the server.
+    That can be told to listen on a port, which will happen at regular intervals after you call L{tornado.ioloop.IOLoop.instance().start()}
+"""
 if __name__ == "__main__":
     app = tornado.httpserver.HTTPServer(Application());
     app.listen(8888);
--- Controller/BaseHandler.py
+++ Controller/BaseHandler.py
@@ -3,6 +3,20 @@ from tornado import gen;
 import concurrent.futures;
 
 class BaseHandler(tornado.web.RequestHandler):
+    """
+        This is the base class for all of the L{Request Handlers<tornado.web.RequestHandler>} used by the app.
+        It supplies a L{concurrent.futures.ThreadPoolExecutor} and handles http errors in L{write_error}
+        
+        @cvar thread_pool: To be used by subclasses when they need to execute synchronous code during a co-routine.
+        @type thread_pool: L{concurrent.futures.ThreadPoolExecutor}
+        
+        @cvar status_messages_4xx: A dictionary of status messages to use for when a http status error is raised.
+        @type status_messages_4xx: L{dict}
+        
+        @cvar status_messages_5xx: A dictionary of status messages to use for when a http status error is raised.
+        @type status_messages_5xx: L{dict}
+    """
+
     thread_pool = concurrent.futures.ThreadPoolExecutor(4);
     
     status_messages_4xx = {
@@ -24,21 +38,47 @@ class BaseHandler(tornado.web.RequestHandler):
     
     # Handle Errors
     def write_error(self, status_code, **kwargs):
-        massage = "";
+        """
+            Handles http status errors that get raised by the application.
+            
+            Overrides L{RequestHandler.write_error}
+            
+            @param status_code: The http status code that was raised.
+            @type status_code: integer
+            
+            @param **kwargs: other arguments (see U{http://www.tornadoweb.org/en/stable/web.html#tornado.web.RequestHandler.write_error} for details)
+            @type **kwargs: dict
+        """
+        
+        # A message to pass to the view template
+        message = "";
+        
+        # Get the first digit of the status code. This will determine the type of code that's been raised.
         firstDigit = int(str(status_code)[0]);
         
+        # Set the correct message based on the first digit of the code
+        
+        # 1xx - Informational
         if firstDigit is 1:
             message = "As you were.";
+        # 2xx - Request successful
         elif firstDigit is 2:
             message = "We did it!";
+        # 3xx - Redirect
         elif firstDigit is 3:
             message = "Time to go...";
+        # 4xx - Request error
         elif firstDigit is 4:
             message = BaseHandler.status_messages_4xx.get(status_code, 'Something went wrong.');
+        # 5xx - Server error
         elif firstDigit is 5:
             message = BaseHandler.status_messages_5xx.get(status_code, 'it\'s brokeded.');
+        # Everything else
         else:
             message = "Well that's strange.";
         
+        # Set response header
         self.set_header('Content-Type', 'text/html');
+        
+        # Render the template view for errors, pass in the page title and message to display.
         self.render('error.html', page_title=message, message=message);
